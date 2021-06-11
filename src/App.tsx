d3f5f690b99cd50feed07d50b394e9e8d84377dd
Fabrizio Ervini <faber.rvn@gmail.com>
2021-06-11 12:50:14 +0200

    feat: fetch current weather

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import icons from './Icons';
+
+interface IWeatherData {
+  name: string;
+  sys: {
+    country: string;
+    sunrise: string;
+    sunset: string;
+  };
+  weather: weather[];
+  main: {
+    temp: number;
+    temp_min: number;
+    temp_max: number;
+    humidity: number;
+  };
+}
+
+type weather = {
+  icon: string;
+  description: string;
+};
 
 function App() {
   const [coords, setCoords] = useState({
     latitude: 0,
     longitude: 0,
   });
-  const [apiData, setApiData] = useState({});
+  const [apiData, setApiData] = useState<IWeatherData>();
 
   useEffect(() => {
     const fetchApi = async () => {
@@ -20,12 +40,7 @@ function App() {
         });
       });
 
-      let url = `${BASE_URL}/weather?q=Barcelona&appid=${process.env.REACT_APP_API_KEY}`;
-
-      // if ('geolocation' in navigator) {
-      //   url = `${BASE_URL}/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${process.env.REACT_APP_API_KEY}`;
-      // }
-      // url = `${BASE_URL}/weather?q=London,uk&appid=${process.env.REACT_APP_API_KEY}`;
+      let url = `${BASE_URL}/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${process.env.REACT_APP_API_KEY}`;
 
       await fetch(url)
         .then(res => res.json())
@@ -36,32 +51,36 @@ function App() {
     fetchApi();
   }, [coords.latitude, coords.longitude]);
 
-  // const type = apiData.weather[0].description | null;
-
-  return apiData.weather ? (
+  return apiData?.weather ? (
     <div className="container">
       <div className="location text-center">
         <p>{`${apiData.name}, ${apiData.sys.country}`}</p>
-      </div>
-      <div className="notification">
-        <p className="text-center"></p>
+        <p>{apiData.sys.sunrise}</p>
+        <p>{apiData.sys.sunset}</p>
       </div>
       <div className="weather-container">
         <div className="weather-icon">
-          {apiData.weather && (
-            <img
-              src={icons[`${apiData.weather[0].description}`]}
-              alt="weather"
-            />
-          )}
+          <img
+            src={`http://openweathermap.org/img/wn/${apiData.weather[0].icon}@4x.png`}
+            alt="weather"
+          />
+        </div>
+        <div className="temperature-description text-center">
+          <p>{apiData.weather[0].description}</p>
         </div>
         <div className="temperature-value text-center">
           <p>
             {Math.floor(apiData.main.temp) - 273}°<span>C</span>
           </p>
-        </div>
-        <div className="temperature-description text-center">
-          <p>{apiData.weather[0].description}</p>
+          <span>
+            <span>Min </span>
+            {Math.floor(apiData.main.temp_min - 273)}°<span>C</span>
+          </span>
+          <span>
+            <span>Max </span>
+            {Math.floor(apiData.main.temp_max - 273)}°<span>C</span>
+          </span>
+          <span>{apiData.main.humidity}%</span>
         </div>
       </div>
     </div>
---
+++ src/types.d.ts
@@ -0,0 +1,49 @@
+export interface Weather {
+  description: string;
+  icon: string;
+  id: number;
+  main: string;
+}
+
+export interface IWeatherData {
+  base: string;
+  clouds: {
+    all: number;
+  };
+  cod: number;
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  dt: number;
+  id: number;
+  main: {
+    feels_like: number;
+    humidity: number;
+    pressure: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  name: string;
+  sys: {
+    country: string;
+    id: number;
+    sunrise: number;
+    sunset: number;
+    type: number;
+  };
+  timezone: number;
+  visibility: number;
+  weather: Weather[];
+  wind: {
+    speed: number;
+    deg: number;
+  };
+}
+
+type ApiDataType = {
+  message: string;
+  status: string;
+  data: IWeatherData;
+};
